d57d2fb7039468478b2db83b799b0e16a658e3eb
chriss1525 <okothchris15@gmail.com>
2023-09-28 10:45:03 +0300

    resolve multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+      console.log(values[0].body, values[1].body.firstName, values[1].body.lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
