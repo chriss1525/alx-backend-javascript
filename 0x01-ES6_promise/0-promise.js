f6eaa4e5070a8ae840bbc961a4263b90dadd4f06
chriss1525 <okothchris15@gmail.com>
2023-09-28 09:59:44 +0300

    return a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,11 @@
-function getResponseFromAPI(success) {
+function getResponseFromAPI(success = true) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve('success');
     } else {
       reject(Error('error'));
-  }
+    }
   });
 }
+
+export default getResponseFromAPI;
