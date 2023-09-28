f133b7b3ff0e490067aac9cf66dac281a1e9777a
chriss1525 <okothchris15@gmail.com>
2023-09-28 11:27:26 +0300

    function that calls two other functions and resolves the different promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === 'rejected') {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+          value.status = 'error';
+        }
+      }
+      return values;
+    });
 }
