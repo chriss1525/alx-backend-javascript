9f53e1a8c070c60c737b766d0e61aadfd1113d32
chriss1525 <okothchris15@gmail.com>
2023-10-04 02:24:07 +0300

    update all items with an intial quantity of 1

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+// function that returns an updated map for all items with initial quantity of 1
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+}
