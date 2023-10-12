f298395da064be9d42e4edca4b78cf486a15f544
chriss1525 <okothchris15@gmail.com>
2023-10-12 19:30:01 +0300

    fix eslint error

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -2,7 +2,7 @@ function signUpUser(firstName, lastName) {
   return new Promise((resolve, reject) => {
     resolve({
       firstName,
-      lastName
+      lastName,
     });
   });
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      for (const value of values) {
-        if (value.status === 'rejected') {
-          value.value = `Error: ${value.reason.message}`;
-          delete value.reason;
-          value.status = 'error';
-        }
-      }
-      return values;
-    });
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+  });
 }
