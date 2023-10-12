20ab46f645ddbc06b0d3d621994986886b02989d
chriss1525 <okothchris15@gmail.com>
2023-10-12 19:32:32 +0300

    fix eslint error

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,8 @@
 function signUpUser(firstName, lastName) {
   return new Promise((resolve, reject) => {
+    if (!firstName || !lastName) {
+      reject(new Error('You need to provide first and last name'));
+    }
     resolve({
       firstName,
       lastName,
