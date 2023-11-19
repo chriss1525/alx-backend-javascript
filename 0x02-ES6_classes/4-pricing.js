13385025beeb120524e65d80e52f71bf04551e9e
chriss1525 <okothchris15@gmail.com>
2023-11-19 11:00:36 +0300

    implement pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,32 +2,32 @@ import Currency from './3-currency';
 
 // create pricing class
 export default class Pricing {
-  constructor (amount, currency) {
+  constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  get amount () {
+  get amount() {
     return this._amount;
   }
 
-  get currency () {
+  get currency() {
     return this._currency;
   }
 
-  set amount (amount) {
+  set amount(amount) {
     this._amount = amount;
   }
 
-  set currency (currency) {
+  set currency(currency) {
     this._currency = currency;
   }
 
-  displayFullPrice () {
+  displayFullPrice() {
     return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
   }
 
-  static convertPrice (amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
