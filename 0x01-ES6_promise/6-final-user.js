54c0f8ed4f3e344e1cc734e89aa6771ac81d2a2a
chriss1525 <okothchris15@gmail.com>
2023-11-19 10:04:08 +0300

    call two other functions in an async function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,23 @@
-import signupUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.allSettled([
-    signupUser(firstname, lastname),
-    uploadPhoto(filename),
-  ]).then((results) => results => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason, 
-    }));
-  });
-}
+// call previous functions.
+// return an array with the following structure:
+// [
+//   {
+//   status: status_of_the_promise,
+//   value: value or error returned by the Promise
+//   },
+//   ...
+//   ]
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const arr = [];
+      results.forEach((result) => {
+        arr.push(result);
+      });
+      return arr;
+    });
+  }
