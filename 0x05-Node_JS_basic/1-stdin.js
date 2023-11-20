6c114fb6fb0eaafba7bccba82d158fdc10c9793c
chriss1525 <okothchris15@gmail.com>
2023-11-21 01:07:47 +0300

    program that is executed through the commandline

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,13 +7,13 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Welcome to Holberton School, what is your name? \n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  rl.close();
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (name) => {
+  console.log(`Your name is: ${name}`);
 });
 
 rl.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
-
