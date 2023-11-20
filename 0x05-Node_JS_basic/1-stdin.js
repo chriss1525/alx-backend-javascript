4d489dd36d17adcf28e6f1ba75c35e00a7d3ad5b
chriss1525 <okothchris15@gmail.com>
2023-11-20 19:30:07 +0300

    add readme

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+// program that is executed through the command line
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name? \n', (answer) => {
+  console.log(`Your name is: ${answer}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
+
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,3 @@
+# Node.js basics
+
+Initialise a node/express app
