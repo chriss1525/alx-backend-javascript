53e6fc458ac1c7ecf77d381ccc8d973726e2b162
chriss1525 <okothchris15@gmail.com>
2023-11-23 23:30:18 +0300

    test cases for simple sum function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -13,5 +13,35 @@ describe('calculateNumber', () => {
     assert.strictEqual(num(-1.2, -3.7), -5);
     assert.strictEqual(num(-1.5, 3.7), 3);
   });
+
+  it('rounds a correctly', () => {
+    assert.strictEqual(num(1.5, 3), 5);
+    assert.strictEqual(num(1.4, 3), 4);
+  });
+
+  it('rounds b correctly', () => {
+    assert.strictEqual(num(1.5, 3), 5);
+    assert.strictEqual(num(1.4, 3), 4);
+  });
+
+  it('rounds a and b correctly', () => {
+    assert.strictEqual(num(1.4, 3.5), 5);
+    assert.strictEqual(num(1.5, 3.5), 6);
+  });
+
+  it('calculates floating point numbers correctly', () => {
+    assert.strictEqual(num(1.0, 2.0), 3);
+    assert.strictEqual(num(1.0, 4.0), 5);
+  });
+
+  it('rounds down correctly', () => {
+    assert.strictEqual(num(1.2, 3.4), 4);
+    assert.strictEqual(num(1.3, 4.1), 5);
+  });
+
+  it('rounds up correctly', () => {
+    assert.strictEqual(num(1.5, 3.5), 6);
+    assert.strictEqual(num(1.6, 3.6), 6);
+  });
 });
 
