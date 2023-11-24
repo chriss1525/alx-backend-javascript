2b4aa8fd2f2173e26bd9fd86c14fee9683f198db
chriss1525 <okothchris15@gmail.com>
2023-11-24 14:20:36 +0300

    tests

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 // fuction that rounds a and b and returns the sum of the rounded numbers
 
 function calculateNumber(a, b) {
-  return Math.ceil(a) + Math.ceil(b);
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const mocha = require('mocha');
 
-const calculateNumber = require('./calcul.js');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('should return sum of integers', () => {
@@ -13,26 +13,26 @@ describe('calculateNumber', () => {
   });
 
   it('should round a', () => {
-    assert.strictEqual(calculateNumber(1.3, 1), 3);
-    assert.strictEqual(calculateNumber(1.7, 1), 3);
-    assert.strictEqual(calculateNumber(1.5, 1), 3);
-    assert.strictEqual(calculateNumber(1.4, 1), 3);
-    assert.strictEqual(calculateNumber(1.6, 1), 3);
+    assert.strictEqual(calculateNumber(1.3, 0), 1);
+    assert.strictEqual(calculateNumber(1.7, 0), 2);
+    assert.strictEqual(calculateNumber(1.5, 0), 2);
+    assert.strictEqual(calculateNumber(1.4, 0), 1);
+    assert.strictEqual(calculateNumber(1.6, 0), 2);
   });
 
   it('should round b', () => {
-    assert.strictEqual(calculateNumber(1, 1.3), 3);
-    assert.strictEqual(calculateNumber(1, 1.7), 3);
-    assert.strictEqual(calculateNumber(1, 1.5), 3);
-    assert.strictEqual(calculateNumber(1, 1.4), 3);
-    assert.strictEqual(calculateNumber(1, 1.6), 3);
+    assert.strictEqual(calculateNumber(0, 1.3), 1);
+    assert.strictEqual(calculateNumber(0, 1.7), 2);
+    assert.strictEqual(calculateNumber(0, 1.5), 2);
+    assert.strictEqual(calculateNumber(0, 1.4), 1);
+    assert.strictEqual(calculateNumber(0, 1.6), 2);
   });
 
   it('should round a and b', () => {
-    assert.strictEqual(calculateNumber(1.3, 1.3), 4);
+    assert.strictEqual(calculateNumber(1.3, 1.3), 2);
     assert.strictEqual(calculateNumber(1.7, 1.7), 4);
     assert.strictEqual(calculateNumber(1.5, 1.5), 4);
-    assert.strictEqual(calculateNumber(1.4, 1.4), 4);
+    assert.strictEqual(calculateNumber(1.4, 1.4), 2);
     assert.strictEqual(calculateNumber(1.6, 1.6), 4);
   });
   });
