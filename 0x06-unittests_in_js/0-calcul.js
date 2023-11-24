74df49e8d8f820ed46af787622d8834f6d8afbd9
chriss1525 <okothchris15@gmail.com>
2023-11-24 13:54:31 +0300

    add tests for the maath.round function

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 // fuction that rounds a and b and returns the sum of the rounded numbers
 
 function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
+  return Math.ceil(a) + Math.ceil(b);
 }
 
 module.exports = calculateNumber;
